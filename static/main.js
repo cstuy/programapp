e054aea42021b215fbaf447c5110049f5cdf8875
Mike Zamansky <zamansky@gmail.com>
2014-01-04 20:03:02 -0500

    no real changes

--- static/main.js
+++ static/main.js
@@ -1,1 +1,21 @@
 console.log("HELLO");
+
+
+var AppRouter = Backbone.Router.extend({
+    routes : {
+	'' 	  : 'home',
+	'newuser' : 'newuser'
+    },
+    home : function() {
+	alert("home");
+    },
+    newuser : function() {
+	alert("newuser");
+    }
+    
+})
+
+var appRouter= new AppRouter();
+
+Backbone.history.start()
+

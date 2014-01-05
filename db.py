493c304a920477dca7ce844046705f3ce0f631aa
Mike Zamansky <zamansky@gmail.com>
2014-01-05 15:39:34 -0500

    added db.py -- only holds email / password so far

---
+++ db.py
@@ -0,0 +1,44 @@
+from pymongo import Connection
+import json,time
+
+
+################################################################################
+#
+#        setting up the database
+#
+################################################################################
+
+c = Connection()
+db = c.programapp
+users = db.users
+
+    
+
+
+################################################################################
+#
+#        User routines
+#
+################################################################################
+
+
+def checkCredentials(username,password):
+    res=users.find({"email":email,"passsword":password})
+    return len([x for x in res])==1
+
+def addUser(email,password):
+    res = users.find({'email':email})
+    if len([x for x in res])>0:
+        return None
+    users.insert({'email':email,'password':password})
+    return (username,password)
+
+
+################################################################################
+#
+#        main (for testing)
+#
+################################################################################
+
+if __name__=='__main__':
+   pass
